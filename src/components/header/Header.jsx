c9eb3408ec7667d4b4766a9d5d1e9cd725195fab
Hira mithani <hira.mithani1.xwave@gmail.com>
2024-09-27 13:35:01 +0100

    2nd commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav m-auto mb-2 mb-lg-0 gap-5">
-                            <li className="nav-item nav-underline">
+                            <li className="nav-item">
                                 <a className="nav-link active text-blcak" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
--- src/components/hero-section/Hero-Section.jsx
+++ src/components/hero-section/Hero-Section.jsx
@@ -3,25 +3,25 @@ import heroImg from "../../assets/heroImg.png";
 
 const HeroSection = () => {
   return (
-    <div className="container d-flex">
-      <div className="text-black mt-5 pt-5">
-        <h2 style={{ marginTop: "200px", fontSize: "50px", color: "#364547" }}>
-          Fair price ride
-        </h2>
-        <h3 style={{ fontSize: "80px" }}>
-          <span style={{ color: "#364547" }}>Rent our</span>
-          <span className="highlight mx-2 fw-bold" style={{ color: "#ef8d21" }}>
-            Scooter
-          </span>
-        </h3>
-        <p style={{ color: "#364547", fontSize: "19px" }}>
-          Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book.
-        </p>
-      </div>
-      <div>
-        <img src={heroImg} alt="heroImg" />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <h4 className="fw-4 " style={{ marginTop: "300px" }}>
+            Fair price ride
+          </h4>
+          <h1 className="fs-1">
+            {" "}
+            Rent our <span className="text-warning">Scooter</span>{" "}
+          </h1>
+          <p>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.{" "}
+          </p>
+        </div>
+        <div className="col-md-6 text-center">
+          <img className="img-fluid" src={heroImg} alt="hero" />
+        </div>
       </div>
     </div>
   );
